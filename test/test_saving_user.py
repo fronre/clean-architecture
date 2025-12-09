2502d7a01cee2f64473e7ec21757bf3ea7869070
fronre <islamhala35@gmail.com>
2025-12-09 23:52:02 +0100

    refactoring

--- test/test_saving_user.py
+++ test/test_saving_user.py
@@ -17,18 +17,18 @@ class UserRepositoryInterface(metaclass=abc.ABCMeta):
 
 class SavingUseCase:
     def __init__(self, user_repository: UserRepositoryInterface):
-        pass
+        self.user_repository: UserRepositoryInterface = user_repository
 
 
-def executed(self, user: User) -> None:
-    pass
+    def executed(self, user: User) -> None:
+        self.user_repository.save()
 
 
-def test_saving_user_is_calling_repository():
+def test_saving_user_is_calling_delegated_repository():
     user: User = User(first_name='Islam', last_name='hala')
     spy_user_repository = Mock(spec=UserRepositoryInterface)
 
-    saving_use_case: SavingUseCase() = SavingUseCase(user_repository=spy_user_repository)
-    saving_use_case.execute(user)
+    saving_use_case: SavingUseCase = SavingUseCase(user_repository=spy_user_repository)
+    saving_use_case.executed(user)
 
     spy_user_repository.save.assert_called_once()
