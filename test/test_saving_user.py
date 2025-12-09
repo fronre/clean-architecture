7a30c3a3d16363e3876f803f58def2119ead625e
fronre <islamhala35@gmail.com>
2025-12-09 22:58:34 +0100

    Red: add test for repository save call in SavingUseCase

--- test/test_saving_user.py
+++ test/test_saving_user.py
@@ -1,2 +1,13 @@
+from unittest.mock import Mock
+
+
 def test_saving_user_is_calling_repository():
+
+
    user: User = User(first_name='Islam', last_name='hala')
+   spy_user_repository: User_repositoryInterface = Mock(spec=UserRepositoryInterface)
+
+   saving_use_case: SavingUseCase() = SavingUseCase(user_repository=spy_user_repository)
+    saving_use_case.executed(user)
+
+    spy_user_repository.assert_called_once()
