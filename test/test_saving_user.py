3b14acf06b0396be61ba068538d9f2ede196556a
fronre <islamhala35@gmail.com>
2025-12-09 23:42:58 +0100

    refactor: extract User, UserRepositoryInterface and SavingUseCase into modules

--- test/test_saving_user.py
+++ test/test_saving_user.py
@@ -2,29 +2,33 @@ import abc
 import dataclasses
 from unittest.mock import Mock
 
+
 @dataclasses.dataclass
 class User:
-     first_name: str
-     last_name: str
+    first_name: str
+    last_name: str
+
 
 class UserRepositoryInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def save(self, user: User) -> None:
-      pass
-
-
-class SavingUseCase:
-
-    def executed(self, user:User) -> None:
         pass
 
 
+class SavingUseCase:
+    def __init__(self, user_repository: UserRepositoryInterface):
+        pass
+
+
+def executed(self, user: User) -> None:
+    pass
+
+
 def test_saving_user_is_calling_repository():
+    user: User = User(first_name='Islam', last_name='hala')
+    spy_user_repository = Mock(spec=UserRepositoryInterface)
 
-   user: User = User(first_name='Islam', last_name='hala')
-   spy_user_repository  = Mock(spec=UserRepositoryInterface)
+    saving_use_case: SavingUseCase() = SavingUseCase(user_repository=spy_user_repository)
+    saving_use_case.execute(user)
 
-   saving_use_case: SavingUseCase() = SavingUseCase(user_repository=spy_user_repository)
-   saving_use_case.executed(user)
-
-   spy_user_repository.save.assert_called_once()
+    spy_user_repository.save.assert_called_once()
