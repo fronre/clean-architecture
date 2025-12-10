32592ee4c02ec8336339b6ea73a95a514d78d640
fronre <islamhala35@gmail.com>
2025-12-10 02:47:25 +0100

    fix(usecase): add optional notification_service and call notify(user) (GREEN)

--- src/Use_Cases/Saving_User/SavingUseCase.py
+++ src/Use_Cases/Saving_User/SavingUseCase.py
@@ -5,13 +5,18 @@ from typing import Optional, Protocol
 
 
 class SavingUseCase:
-    def __init__(self, user_repository: UserRepositoryInterface, authorization_service=None):
+    def __init__(self, user_repository, notification_service=None, authorization_service=None):
         self.user_repository = user_repository
+        self.notification_service = notification_service
         self.authorization_service = authorization_service
 
-    def execute(self, user: User) -> None:
-        if self.authorization_service and not self.authorization_service.is_authorized(user):
-            return
-        self.user_repository.save(user)
 
+def execute(self, user):
+    if self.authorization_service and not self.authorization_service.is_authorized(user):
+        return
+
+    self.user_repository.save(user)
+
+    if self.notification_service:
+        self.notification_service.notify(user)
 
