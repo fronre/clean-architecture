6a88edfd68f5b7112f22e4358cc55679438c2455
fronre <islamhala35@gmail.com>
2025-12-10 03:37:37 +0100

    refactoring

--- src/delivery/fastapi_app.py
+++ src/delivery/fastapi_app.py
@@ -2,11 +2,9 @@
 from fastapi import FastAPI, Depends
 import pydantic
 
-# NOTE: adjust case of use_cases package if your folder is "Use_Cases"
 from src.use_cases.saving_user.saving_use_case import SavingUseCase
 from src.infrastructure.user_repository.in_memory_user_repository import InMemoryUserRepository
 
-# import the interface-adapter controller + presenter
 from src.interface_adapters.controllers.create_user_controller import create_user_controller
 from src.interface_adapters.presenters.user_presenter import UserPresenter
 
@@ -23,5 +21,4 @@ def get_usecase():
 @app.post("/users")
 def create_user(dto: CreateUserDTO, use_case: SavingUseCase = Depends(get_usecase)):
     presenter = UserPresenter()
-    # delegate to controller (adapter) which maps DTO -> domain, calls usecase and presents
     return create_user_controller(dto.dict(), use_case, presenter)
