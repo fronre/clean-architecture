c7f7b35ab3e9de12ea01d271e34ac47b40624c17
fronre <islamhala35@gmail.com>
2025-12-10 02:59:57 +0100

    : feat(presentation): add presenter interface and simple presenter

---
+++ test/test_saving_user_presenter.py
@@ -0,0 +1,8 @@
+from src.presentation.user_presenter import UserPresenter
+from src.entities.user import User
+
+def test_user_presenter_outputs_dict():
+    presenter = UserPresenter()
+    u = User("A", "B")
+    out = presenter.present(u)
+    assert out == {"first_name":"A","last_name":"B"}
