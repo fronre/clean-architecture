2d8b33d81b9a9ce90a582f0e104c98b9c04b39be
fronre <islamhala35@gmail.com>
2025-12-10 01:06:39 +0100

    refactor(tests): remove redundant assertions and assert save called with user

--- test/test_saving_user.py
+++ test/test_saving_user.py
@@ -5,29 +5,29 @@ from src.user import User
 from src.user_repository_interface import UserRepositoryInterface
 
 
+
 def test_saving_user_is_calling_delegated_repository():
-     #Arrange
-    user: User = User(first_name='Islam', last_name='hala')
+    # Arrange
+    user = User(first_name='Islam', last_name='hala')
     spy_user_repository = Mock(spec=UserRepositoryInterface)
 
-    saving_use_case: SavingUseCase = SavingUseCase(user_repository=spy_user_repository)
-    #Act
+    saving_use_case = SavingUseCase(user_repository=spy_user_repository)
+
+    # Act
     saving_use_case.execute(user)
-      #Assert
-    spy_user_repository.save.assert_called_once()
+
+    # Assert
+    spy_user_repository.save.assert_called_once_with(user)
+
 
 def test_saving_user_save_the_user_in_the_repository():
     # Arrange
-
-    user: User = User('Islam', 'hala')
+    user = User('Islam', 'hala')
     spy_user_repository = Mock(spec=UserRepositoryInterface)
-    spy_user_repository.save = Mock()
-    saving_use_case:SavingUseCase= SavingUseCase(user_repository=spy_user_repository)
+    saving_use_case = SavingUseCase(user_repository=spy_user_repository)
 
     # Act
-
     saving_use_case.execute(user)
-
 
     # Assert
     spy_user_repository.save.assert_called_once_with(user)
